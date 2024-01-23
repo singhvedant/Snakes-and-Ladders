b320f5cdb90e12c0d478f48d87fa24bfaa33a9e9
Vedant Singh <singhvedant1701@gmail.com>
2024-01-23 16:06:17 +0530

    Use case 4 : Game loop

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,12 @@ public class Main {
     public static void main(String[] args) {
         Player p1 = new Player("Vedant");
 
+        System.out.println("Game Start");
         System.out.println("Player Position : " + p1.position);
-        p1.rollDie();
+        while (p1.position<100) {
+            p1.rollDie();
+        }
+        System.out.println("Game Finished");
+
     }
 }
