1e3db22cc174680f3c36381d8bfae5bebd114b12
Vedant Singh <singhvedant1701@gmail.com>
2024-01-23 16:00:44 +0530

    Use case 3 : Player options

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Player {
@@ -14,5 +15,33 @@ public class Player {
     public void rollDie() {
         Integer rolled = new Dice().roll();
         System.out.println(this.name + " rolled " + rolled);
+
+        //Check for Random Option
+        int rand = new Random().nextInt(4);
+        switch (rand) {
+            case 1: noPlay(rolled);break;
+            case 2: ladder(rolled);break;
+            case 3: snake(rolled);break;
+        }
+    }
+
+    public void noPlay(int rolled) {
+        System.out.println("No Play!");
+    }
+
+    public void ladder(int rolled) {
+        this.position += rolled;
+        if (this.position>100) {
+            this.position = 100;
+        }
+        System.out.println("Ladder gained. New Position : " + this.position);
+    }
+
+    public void snake(int rolled) {
+        this.position -= rolled;
+        if (this.position<0) {
+            this.position = 0;
+        }
+        System.out.println("Snake Bite. New Position : " + this.position);
     }
 }
